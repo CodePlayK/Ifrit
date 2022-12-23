2c25228c5a10414b3133b43475ca9f871b194f68
Administrator <22>
2022-12-24 03:28:20 +0800

    剔除

--- TEST/Label.gd
+++ TEST/Label.gd
@@ -3,6 +3,7 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print()
 	pass # Replace with function body.
 
 
--- core/script/hitbox.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-var modeflg=true
-func _ready():
-	pass # Replace with function body.
-
-
-func _on_HitBox_body_entered(body):
-	pass # Replace with function body.
